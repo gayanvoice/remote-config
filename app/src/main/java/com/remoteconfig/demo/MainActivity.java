8b819bd1c2b5c29a52a9807d0902d6936b26e33d
gayankuruppu <example@example.com>
2019-10-01 08:58:40 +0530

    Update App

--- app/src/main/java/com/remoteconfig/demo/MainActivity.java
+++ app/src/main/java/com/remoteconfig/demo/MainActivity.java
@@ -8,22 +8,77 @@ import android.widget.TextView;
 
 import com.remoteconfig.library.Remote;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+
+
+    TextView textView, textStatus;
     Remote remote;
+    String StringGetAppURL;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        StringGetAppURL = "https://raw.githubusercontent.com/gayankuruppu/android-remote-config-library/master/remote-config.json";
+
         textView = findViewById(R.id.textView);
+        textStatus = findViewById(R.id.textStatus);
+
+
         remote = new Remote();
+
 
         textView.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                textView.setText(remote.getText());
+
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                queue.getCache().clear();
+
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, StringGetAppURL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String Response) {
+                                textView.setText(Response);
+                                textStatus.setText(R.string.app_response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // on error
+                        textStatus.setText(R.string.app_error);
+                    }
+                });
+
+                queue.add(stringRequest);
+                queue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<String>() {
+                    @Override
+                    public void onRequestFinished(Request<String> request) {
+                       // finished
+                        textStatus.setText(R.string.app_finish);
+                    }
+                });
+
+
+
+
+
+
             }
         });
 
