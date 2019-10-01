62415137e19ab7593f992972db45030d110d382e
gayankuruppu <example@example.com>
2019-10-01 08:30:57 +0530

    Update App

--- app/src/main/java/com/remoteconfig/demo/MainActivity.java
+++ app/src/main/java/com/remoteconfig/demo/MainActivity.java
@@ -3,6 +3,9 @@ package com.remoteconfig.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.remoteconfig.library.Remote;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        Remote remote = new Remote();
+
+        textView.setText(remote.getText());
+
     }
 }
---
+++ library/src/main/java/com/remoteconfig/library/Remote.java
@@ -0,0 +1,7 @@
+package com.remoteconfig.library;
+
+public class Remote {
+    public int getText(){
+        return R.string.app_name;
+    }
+}
