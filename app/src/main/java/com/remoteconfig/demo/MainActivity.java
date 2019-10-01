72da8822f4d7adc4c46611fa9a34f7522596be61
gayankuruppu <example@example.com>
2019-10-01 08:37:30 +0530

    Update App

--- app/src/main/java/com/remoteconfig/demo/MainActivity.java
+++ app/src/main/java/com/remoteconfig/demo/MainActivity.java
@@ -3,21 +3,31 @@ package com.remoteconfig.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.remoteconfig.library.Remote;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Remote remote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
-        Remote remote = new Remote();
+        textView = findViewById(R.id.textView);
+        remote = new Remote();
 
-        textView.setText(remote.getText());
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                textView.setText(remote.getText());
+            }
+        });
+
+
 
     }
 }
